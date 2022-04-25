01db560e6587f89edff93c39b1e5a34776b12d4e
ArkaGoel <77157313+ArkaGoel@users.noreply.github.com>
2022-04-25 18:16:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,27 @@ function preload(){
 
 }
 function setup(){
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(395, 300);
     canvas.center()
+    video = createCapture(VIDEO);
+    video.size(395, 300)
+    video.hide();
+
+    PoseNET = ml5.poseNet(video, modelLoaded);
+    function modelLoaded(){
+        console.log("PoseNET Initialized!");
+    }
+    PoseNET.on('pose', gotPoses)
+    function gotPoses(results){
+        if(results.length > 0){
+            console.log(results);
+            console.log("nose x =" + results[0].pose.nose.x);
+            console.log("nose y =" + results[0].pose.nose.y);
+        }
+    }
 }
 function draw(){
-
+ image(video, 0, 0, 395, 300)
 }
 function take_snapshot(){
     file = document.getElementById("file_name").value;
